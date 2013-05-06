dc032e62d46620c3be38a96a90af2d2583025295
Brandon Behrens <brandonbehrens@gmail.com>
2013-05-06 17:03:54 -0500

    Adding some things to SearchDocument

--- AmazingCloudSearch/Contract/SearchDocument.cs
+++ AmazingCloudSearch/Contract/SearchDocument.cs
@@ -1,15 +1,18 @@
-﻿using System.Web.Script.Serialization;
-
-namespace AmazingCloudSearch.Contract
+﻿namespace AmazingCloudSearch.Contract
 {
     public class SearchDocument : ISearchDocument
-    {
-        [ScriptIgnoreAttribute]
+    {        
         public string id { get; set; }
+        public string text_relevance { get; set; }
+        public string domain { get; set; }
+        public string tenant { get; set; }
     }
 
     public interface ISearchDocument
     {        
         string id { get; set; }
+        string text_relevance { get; set; }
+        string domain { get; set; }
+        string tenant { get; set; }
     }
 }
